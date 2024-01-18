70a8e03b6cb20f93d1318877eef06ac55809a44a
Yushi_np28@top-academy.ru <Yushi_np28@top-academy.ru>
2024-01-18 19:36:17 +0300

    Добавьте файлы проекта.

---
+++ Registration_and_Autorization/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Registration_and_Autorization
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RegistrationForm registration;
+        AurizationForm aurization;
+        public MainWindow()
+        {
+            InitializeComponent();
+            registration = new RegistrationForm(
+              TextLogin, TextPassword
+              );
+
+            aurization = new AurizationForm(
+                TextLogin, TextPassword
+                );
+        }
+        private void AvtorizButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (AvtorizButton.Content.ToString() == "Уже есть аккаунт")
+            {
+                LabelRegisrt.Content = "Авторизация";
+                RegButton.Content = "Войти";
+                AvtorizButton.Content = "Назад";
+            }
+            else
+            {
+                LabelRegisrt.Content = "Регистрация";
+                RegButton.Content = "Зарегестрироваться";
+                AvtorizButton.Content = "Уже есть аккаунт";
+            }
+        }
+        private bool Str(string str )
+        {
+            if (str.Length>0 && str.IndexOfAny(new char[] { '/', '*', '+', '-', '&', '?', '.', ',', ' ', '!', '#', '$', '%', '^', '(', ')', '=', '<', '>' })==-1)
+            {
+                return true;
+            }
+            return false;
+        }
+        private void RegButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(Str(TextLogin.Text) && Str(TextPassword.Password))
+            {
+                if (RegButton.Content.ToString() == "Зарегестрироваться")
+                {
+                    if (registration.execute("users.txt"))
+                    {
+                        MessageBox.Show("Удачная регистрация");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Регистрация не удалась");
+                    }
+                }
+                else
+                {
+                    if (aurization.execute("users.txt"))
+                    {
+                        MessageBox.Show($"Добрый день, {TextLogin.Text}");
+                        //Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неверно введенные данные");
+                    }
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Запрещенный символ!");
+            }
+        }
+    }
+}
---
+++ Registration_and_Autorization/User.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Registration_and_Autorization
+{
+    abstract class UserForm
+    {
+        protected TextBox login;
+        protected PasswordBox password;
+
+        public UserForm(TextBox login, PasswordBox password)
+        {
+            this.login = login;
+            this.password = password;
+        }
+
+        abstract public bool execute(string path);
+    }
+    class RegistrationForm : UserForm
+    {
+        public RegistrationForm(TextBox login, PasswordBox password) :
+            base(login, password)
+        { }
+
+        public override bool execute(string path)
+        {
+            string textLogin = login.Text;
+
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string s;
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        string log = s.Split(' ')[0];
+                        if (log == textLogin)
+                            return false;
+
+                    }
+                }
+            }
+
+            int hashPassword = password.Password.GetHashCode();
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine($"{textLogin} {hashPassword}");
+            }
+            return true;
+        }
+    }
+
+    class AurizationForm : UserForm
+    {
+        public AurizationForm(TextBox login, PasswordBox password) :
+           base(login, password)
+        { }
+
+        public override bool execute(string path)
+        {
+            string textLogin = login.Text;
+            string textHashPassword = password.Password.GetHashCode().ToString();
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string s;
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        string[] data = s.Split(' ');
+                        if (data[0] == textLogin && data[1] == textHashPassword)
+                            return true;
+                    }
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
